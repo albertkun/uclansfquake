4559e50f3fdacefdfbeb20ded460273e797380b7
albertkun <albertk@gmx.com>
2019-05-02 20:35:04 -0700

    re-add project files

---
+++ models.py
@@ -0,0 +1,42 @@
+# Import from peewee
+from peewee import *
+from flask import session
+
+# load our config file
+from config import Config
+
+# Connect to the PostgresqlDatabase
+db = PostgresqlDatabase(database=Config.DATABASE, user=Config.USERNAME, password=Config.SECRET_KEY,
+host=Config.HOST, port=Config.PORT)
+
+db.connect()
+
+class Earthquake(Model):
+    idearthquake = IntegerField(primary_key=True)
+    name = CharField()
+    magnitude = DecimalField()
+    latepc = DecimalField()
+    longepc = DecimalField()
+    date = DateTimeField()
+
+    class Meta:
+        database = db
+        db_table = 'earthquakes'
+
+class Response(Model):
+    idsite = IntegerField(primary_key=True)
+    idearthquake = IntegerField()
+    idbuilding = IntegerField()
+    sdr = DecimalField()
+    pfa = DecimalField()
+    idresponse = IntegerField()
+    class Meta:
+        database = db
+        db_table = 'responses'
+
+class Building(Model):
+    idbuilding = IntegerField(primary_key=True)
+    level = IntegerField()
+    class Meta:
+        database = db
+        db_table = 'buildings'    
